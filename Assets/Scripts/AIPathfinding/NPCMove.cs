6d3f80be7690a87331dcc4b58ae964dd8d036f1b
The-Raider <TheRaider1596@users.noreply.github.com>
2020-07-14 04:24:12 +0200

    Added basic NavMesh to tester level
    Made a base nav mesh to test different ai behaviour in the tester level. Currently, the AI chases the player while avoiding the obstacles on the map.

---
+++ Assets/Scripts/AIPathfinding/NPCMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMove : MonoBehaviour
+{
+    [SerializeField] private Transform _destination;
+
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetDestination();
+    }
+
+    private void SetDestination()
+	{
+        if(_destination != null)
+		{
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+		}
+	}
+}
