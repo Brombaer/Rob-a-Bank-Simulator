2764de292824c5a047138ec8676a76a26f156964
The-Raider <TheRaider1596@users.noreply.github.com>
2020-07-14 18:48:01 +0200

    Made basic patrol script that allows an NPC to walk back and forth between two points

---
+++ Assets/Scripts/AIPathfinding/NPCMoveBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.AIPathfinding
+{
+	public abstract class NPCMoveBase : MonoBehaviour
+	{
+		[SerializeField] protected Transform _destination;
+
+		[SerializeField] protected NavMeshAgent _navMeshAgent;
+
+		private void Start()
+		{
+
+		}
+
+		private void Update()
+		{
+			SetDestination();
+		}
+
+		protected abstract void SetDestination();
+	}
+}
---
+++ Assets/Scripts/AIPathfinding/Waypoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.AIPathfinding
+{
+	public class Waypoint : MonoBehaviour
+	{
+		[SerializeField] protected float degubDrawRadius = 1.0f;
+
+		public virtual void OnDrawGizmos()
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(transform.position, degubDrawRadius);
+		}
+	}
+}
