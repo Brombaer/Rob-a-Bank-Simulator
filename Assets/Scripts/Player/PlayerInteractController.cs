e64a3edf35b3cb0ab6c674ff5cbf95dc562131c4
Dustin <dustin.behnke@t-online.de>
2020-08-08 17:40:46 +0200

    Implemented outline for interactable objects regarding distance and mouse position

--- Assets/Scripts/Player/PlayerInteractController.cs
+++ Assets/Scripts/Player/PlayerInteractController.cs
@@ -7,7 +7,9 @@ public class PlayerInteractController : MonoBehaviour
     public Transform Camera;
 
     [SerializeField]
-    private float _maxDistance = 2;
+    private float _maxInteractDistance = 2;
+    [SerializeField]
+    private float _maxOutlineDistance = 5;
     [SerializeField]
     private Transform _interactionCenter;
 
@@ -20,20 +22,26 @@ public class PlayerInteractController : MonoBehaviour
 
     public float WalletAmount { get => PlayerWallet; }
 
-    private void Awake()
-    {
 
-    }
+    private OutlineController _prevController;
+    private OutlineController _currentController;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        InteractWithObject();
+        OutlineInteractableObject();
+    }
+
+    private void InteractWithObject()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             RaycastHit raycastHit;
 
             if (Physics.Raycast(Camera.position, Camera.forward, out raycastHit))
             {
-                if ((raycastHit.point - _interactionCenter.position).magnitude < _maxDistance)
+                if ((raycastHit.point - _interactionCenter.position).magnitude < _maxInteractDistance)
                 {
                     var interactable = raycastHit.transform.GetComponent<Interactable>();
 
@@ -56,9 +64,55 @@ public class PlayerInteractController : MonoBehaviour
         }
     }
 
+    private void OutlineInteractableObject()
+    {
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(Camera.position, Camera.forward, out raycastHit, _maxOutlineDistance))
+        {
+            if (raycastHit.collider.CompareTag("CollectableItem") && raycastHit.collider != null)
+            {
+                _currentController = raycastHit.collider.GetComponent<OutlineController>();
+
+                if (_prevController != _currentController)
+                {
+                    HideOutline();
+                    ShowOutline();
+                }
+
+                _prevController = _currentController;
+            }
+            else
+            {
+                HideOutline();
+            }
+        }
+        else
+        {
+            HideOutline();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         SafedMoney = PlayerWallet;
         PlayerWallet = 0;
+    }
+
+    private void ShowOutline()
+    {
+        if (_currentController != null)
+        {
+            _currentController.ShowOutline();
+        }
+    }
+
+    private void HideOutline()
+    {
+        if (_prevController != null)
+        {
+            _prevController.HideOutline();
+            _prevController = null;
+        }
     }
 }
