8a4489dd5c4442cfa384a3ac79de7a12f6b42b75
Egor <e.ageev@saedubai.com>
2020-08-21 01:02:43 +0200

    Added: Hurtscreen, organized files, fixed shake screen

---
+++ Assets/Scripts/UI/CountDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class CountDown : MonoBehaviour
+{
+	[SerializeField] private Alarm _alarm;
+	private TextMeshProUGUI _text;
+
+
+	private void Awake()
+	{
+		_text = GetComponent<TextMeshProUGUI>();
+	}
+
+	private void Update()
+	{
+		if(_alarm.SpawnDelay > 0)
+		{
+			int newTime = Mathf.RoundToInt(_alarm.SpawnDelay);
+			_text.text = $"{newTime}s left";
+		}
+		else
+		{
+			_text.text = $"Police force has arrived!";
+		}
+	}
+}
