0b0756ae6fac87cec4c6d7c0a201ec2a19f3212b
Egor <e.ageev@saedubai.com>
2020-08-25 18:44:09 +0200

    Added: FMOD Project

---
+++ Assets/Scripts/FMODPlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODPlay : MonoBehaviour
+{
+	[FMODUnity.EventRef]
+	[SerializeField] private string _amb;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		FMODUnity.RuntimeManager.PlayOneShot(_amb, gameObject.transform.position);
+	}
+}
